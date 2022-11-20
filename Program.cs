6c9ee54c21edc44c8ae20b81c8cd69a3f13c4964
Felix Wieser <felix.a.wieser@gmail.com>
2022-11-20 16:55:14 +0100

    Noch ein paar Sätze

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿Console.WriteLine("Wie heißt du?");
 var name = Console.ReadLine();
 Console.WriteLine("Hallo, " + name + "!");
+Console.WriteLine("Danke");
+Console.WriteLine("Bis zum nächsten Mal!");
+Console.WriteLine("Felix der programmierer!");
+Console.WriteLine("Elias ist auch ein prorammierer");
